6fe9f89abd5f81c05a524585e793b1c967b54b42
Ghulam Mustafa <mustafa629@gmail.com>
2024-09-24 12:27:05 +0500

    page file update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import {client} from "../sanity/lib/client";
 import { urlFor } from '@/sanity/lib/image';
+import Image from "next/image";
+type BlogType = {
+  _id: string;
+  title: string;
+  description: string;
+  image: string;
+};
 const getBlogs=async()=>{
   const blogs=await client.fetch(`*[_type=="blog"]`);
   console.log(blogs);
@@ -14,11 +21,17 @@ export default async function Home() { const blogs= await getBlogs();
       |<h1 className="text-3xl font-bold">Blogs</h1>|
     </div>
     <div className="grid grid-cols-3 gap-4">
-      {blogs.map((blog)=>(
+      {blogs.map((blog:BlogType)=>(
         <div key={blog._id} className="border p-4">
           <h2 className="text-xl font-bold">{blog.title}</h2>
           <p className="text-gray-500">{blog.description}</p>
-          <img src={urlFor(blog.image).width(200).url()}/>
+        
+         <Image
+              src={urlFor(blog.image).url()}
+              height={400}
+              width={400}
+              alt="img-blog"
+            />
         </div>))
     }
     </div>
